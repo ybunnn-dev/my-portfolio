7d1eaf2d13460637c99ffd3b9beb6c91d70b0005
John Ivan Belaro <belaroivan17@gmail.com>
2026-03-29 18:46:57 +0800

    fixed educational background title

--- app/sections/education.tsx
+++ app/sections/education.tsx
@@ -1,7 +1,7 @@
 export default function Education() {
   return (
     <div className="bg-white dark:bg-[#1E293B] rounded-lg p-6 border border-gray-200 dark:border-gray-700 text-header_text dark:text-gray-300 transition-colors duration-300 h-full">
-      <h2 className="font-semibold text-xl mb-3 text-gray-800 dark:text-white">Education Background</h2>
+      <h2 className="font-semibold text-xl mb-3 text-gray-800 dark:text-white">Educational Background</h2>
 
       <div className="bg-gray-100 dark:bg-gray-50/20 rounded-lg p-6 flex items-center gap-3 mt-2">
           <svg viewBox="0 0 24 24" className="text-gray-600 dark:text-white w-16 h-16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.93066 15.3551V14.5763L11.8045 16.6651L16.6783 14.5763V15.3551L11.8045 17.6429L6.93066 15.3551Z" fill="currentColor"stroke="currentColor"strokeWidth="2.78505" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M11.5763 12.0508L19.1525 9.16458L11.5763 7L4 9.16458L11.5763 12.0508Z" fill="#000000"></path> <path d="M4 9.16458L11.5763 7L19.1525 9.16458L11.5763 12.0508L4 9.16458ZM4 9.16458V12.0508" stroke="currentColor"strokeWidth="2.78505" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
